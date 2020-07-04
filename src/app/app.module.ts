93789b6e3ac57e1313111f9e86349b3711d13de9
harrymkwn <heratmakwana19@gmail.com>
2020-07-04 07:21:53 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonformComponent } from './personform/personform.component';
+import { HelpComponent } from './help/help.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonformComponent,
+    HelpComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
